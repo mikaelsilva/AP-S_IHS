c55816a78ff1e57b3e857316a3585e54c79bc254
douglas <jdps@cin.ufpe.br>
2018-10-22 13:38:11 -0300

    c

--- AP4/ap4.asm
+++ AP4/ap4.asm
@@ -1,14 +1,16 @@
-extern printf
-section .data
+;programa calcula (pi/4) == sum (-1)^i/(2i+1)
+
+extern printf ;declarando a função printf extern para uso no assembly
+section .data ;declaração de variaveis inicializadas
 value: dq 0.0
 sinal: dq -1.0
 s: dq -1.0
 
-aux: dd -1
-str: db 'Valor = %f',13, 10, 0
-str2: db 'Valor = %d',13, 10, 0
-section .text
-global main
+aux: dd -1	;contador para o i
+str: db 'Valor = %f',13, 10, 0	;argumento do printf
+str2: db 'Valor = %d',13, 10, 0 ;argumento do printf
+section .text	;inicio do codigo
+global main	
 
 main:
 
@@ -17,36 +19,36 @@ mov eax, -1
 
 
 cont:
-
-fld qword[sinal]
+				;nessa parte eu guardo o sinal antigo e multiplico por -1 pra achar o novo
+fld qword[sinal];o sinal para controlar a variação (-1)^i
 fmul qword[s]
 fstp qword[sinal]
 
 
-push eax
+push eax		;salvo eax por que o printf tem um retorno que o sobreescreve
 
 
-mov eax, dword[aux]
+mov eax, dword[aux]	 ;calculo o incremento do i
 inc eax
 mov dword[aux], eax
 
-fild dword[aux]
-fild dword[aux]
-faddp
-fld1
-faddp
-fld qword[sinal]
-fdivrp
-fadd qword[value]
+fild dword[aux] 	;carrego i na fpu
+fild dword[aux]		
+faddp				;fpu agora possui 2i
+fld1				
+faddp				;2i + 1
+fld qword[sinal]	
+fdivrp				;divido por ((-1)^i)/(2i+1)
+fadd qword[value]	;somo o novo passo ao valor calculado anteriormente
 
 fst qword[value]
 
 
-push dword[value+4]
-push dword[value]
-push dword str
-call printf
-add esp, 12
+push dword[value+4]		;passo o final do valor
+push dword[value]		;passo o inicio do valor
+push dword str			;passo o argumento do printf
+call printf				;chamo o printf
+add esp, 12				;necessário para rearrumar a pilha do contrário ela ia ficar com o inicio 							;zaudo
 
 pop eax
 inc eax
